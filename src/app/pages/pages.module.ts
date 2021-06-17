8adec53b3f8c9837c920f5204901d7594f30ed95
Mário Macedo <mario.macedo@trsolucoes.com>
2021-06-16 21:25:19 -0300

    feat: home page with links

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PagesComponent } from './pages.component';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [PagesComponent],
+  declarations: [HomeComponent],
+  imports: [CommonModule, RouterModule],
 })
 export class PagesModule {}
