1d54b1c9bc83f84d6310c7f4319faeef48fdcf65
Mário Macedo <mario.macedo@trsolucoes.com>
2021-06-16 21:59:49 -0300

    feat: response question four

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,10 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { DividerModule } from 'primeng/divider';
 import { HomeComponent } from './home/home.component';
+import { QuestionFiveComponent } from './question-five/question-five.component';
+import { QuestionFourComponent } from './question-four/question-four.component';
+import { QuestionOneComponent } from './question-one/question-one.component';
+import { QuestionSixComponent } from './question-six/question-six.component';
+import { QuestionThreeComponent } from './question-three/question-three.component';
+import { QuestionTwoComponent } from './question-two/question-two.component';
 
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [CommonModule, RouterModule],
+  declarations: [
+    HomeComponent,
+    QuestionOneComponent,
+    QuestionTwoComponent,
+    QuestionThreeComponent,
+    QuestionFourComponent,
+    QuestionFiveComponent,
+    QuestionSixComponent,
+  ],
+  imports: [CommonModule, RouterModule, DividerModule],
 })
 export class PagesModule {}
